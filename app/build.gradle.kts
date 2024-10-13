29f0475b1946889ceff4eea2301ea743e7666cfb
Sergio <63758513+Unshesho@users.noreply.github.com>
2024-10-13 15:24:02 -0300

    add: config koin (#2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,22 @@ android {
 
 dependencies {
 
+    //Core Dependencies
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //Network Dependencies
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
+    //DI Dependencies
+    implementation("io.insert-koin:koin-core:4.0.0")
+    implementation("io.insert-koin:koin-android4.0.0")
+
+    //Test Dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
